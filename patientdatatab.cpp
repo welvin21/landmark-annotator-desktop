06a01b7da65dee5d5d53673a60a1658476eca57b
Welvin Bun <welvin07@gmail.com>
2021-12-29 17:40:47 +0800

    Enable patient data saving feature

--- patientdatatab.cpp
+++ patientdatatab.cpp
@@ -1,71 +1,111 @@
 #include "patientdatatab.h"
 
 PatientDataTab::PatientDataTab(DesktopApp* parent) {
-    QObject::connect(parent->ui.savePatientButton, &QPushButton::clicked, [parent]() {
+    this->parent = parent;
+
+    QObject::connect(parent->ui.savePatientButton, &QPushButton::clicked, [this]() {
         bool isPatientDataValid = true;
         
         std::string name, hkid, phone, email, medicalNumber, nationality, address;
 
-        if ((name = parent->ui.nameInput->toPlainText().toStdString()) != "") {
-            parent->patient.setName(name);
+        if ((name = this->parent->ui.nameInput->toPlainText().toStdString()) != "") {
+            this->parent->patient.setName(name);
         }
         else isPatientDataValid = false;
 
 
-        if ((hkid = parent->ui.idInput->toPlainText().toStdString()) != "") {
-            parent->patient.setHKID(hkid);
+        if ((hkid = this->parent->ui.idInput->toPlainText().toStdString()) != "") {
+            this->parent->patient.setHKID(hkid);
         }
         else isPatientDataValid = false;
 
-        if ((phone = parent->ui.phoneInput->toPlainText().toStdString()) != "") {
-            parent->patient.setPhoneNumber(phone);
+        if ((phone = this->parent->ui.phoneInput->toPlainText().toStdString()) != "") {
+            this->parent->patient.setPhoneNumber(phone);
         }
         else isPatientDataValid = false;
 
-        if ((email = parent->ui.phoneInput->toPlainText().toStdString()) != "") {
-            parent->patient.setEmail(email);
+        if ((email = this->parent->ui.phoneInput->toPlainText().toStdString()) != "") {
+            this->parent->patient.setEmail(email);
         }
         else isPatientDataValid = false;
 
-        if ((medicalNumber = parent->ui.phoneInput->toPlainText().toStdString()) != "") {
-            parent->patient.setMedicalNumber(medicalNumber);
+        if ((medicalNumber = this->parent->ui.phoneInput->toPlainText().toStdString()) != "") {
+            this->parent->patient.setMedicalNumber(medicalNumber);
         }
         else isPatientDataValid = false;
 
-        if ((nationality = parent->ui.phoneInput->toPlainText().toStdString()) != "") {
-            parent->patient.setNationality(nationality);
+        if ((nationality = this->parent->ui.phoneInput->toPlainText().toStdString()) != "") {
+            this->parent->patient.setNationality(nationality);
         }
         else isPatientDataValid = false;
 
-        if ((address = parent->ui.phoneInput->toPlainText().toStdString()) != "") {
-            parent->patient.setAddress(address);
+        if ((address = this->parent->ui.phoneInput->toPlainText().toStdString()) != "") {
+            this->parent->patient.setAddress(address);
         }
         else isPatientDataValid = false;
 
-        if (parent->ui.male->isChecked()) parent->patient.setSex(Male);
-        else if (parent->ui.female->isChecked()) parent->patient.setSex(Female);
+        if (this->parent->ui.male->isChecked()) this->parent->patient.setSex(Male);
+        else if (this->parent->ui.female->isChecked()) this->parent->patient.setSex(Female);
         else isPatientDataValid = false;
 
         try {
-            std::string height = parent->ui.heightInput->toPlainText().toStdString();
-            std::string weight = parent->ui.weightInput->toPlainText().toStdString();
+            // Height and weight should be a number
+            std::string height = this->parent->ui.heightInput->toPlainText().toStdString();
+            std::string weight = this->parent->ui.weightInput->toPlainText().toStdString();
 
             float parsedHeight = (float) std::stod(height, nullptr);
             float parsedWeight = (float) std::stod(weight, nullptr);
-            parent->patient.setHeight(parsedHeight);
-            parent->patient.setWeight(parsedWeight);
+            this->parent->patient.setHeight(parsedHeight);
+            this->parent->patient.setWeight(parsedWeight);
         }
         catch (std::exception& ia) {
             isPatientDataValid = false;
         }
 
         if (!isPatientDataValid) {
-            parent->ui.patientDataValidation->setText("Please double check your input.\nNote that height and weight should be a number.");
+            this->parent->ui.patientDataValidation->setText("Please double check your input.\nNote that height and weight should be a number.");
         }
         else {
-            parent->ui.patientDataValidation->setText("All good to go.");
+            if (this->savePatientData()) {
+                this->parent->ui.patientDataValidation->setText("All good to go!");
+            }
+            else {
+                this->parent->ui.patientDataValidation->setText("Something went wrong while saving patient data.");
+            }
         }
 
-        parent->patient.setValidity(isPatientDataValid);
+        this->parent->patient.setValidity(isPatientDataValid);
     });
+}
+
+bool PatientDataTab::savePatientData() {
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save patient data"), QString(), tr("Text (*.txt)"));
+        
+    if (!fileName.isEmpty()) {
+        QFile file(fileName);
+        file.open(QIODevice::WriteOnly | QIODevice::Text);
+
+        QTextStream out(&file);
+        out << "Full name: " << QString::fromStdString(this->parent->patient.getName()) << "\n";
+        out << "HKID: " << QString::fromStdString(this->parent->patient.getHKID()) << "\n";
+        out << "Phone number: " << QString::fromStdString(this->parent->patient.getPhoneNumber()) << "\n";
+        out << "Email: " << QString::fromStdString(this->parent->patient.getEmail()) << "\n";
+        out << "Medical number: " << QString::fromStdString(this->parent->patient.getMedicalNumber()) << "\n";
+        out << "Nationality: " << QString::fromStdString(this->parent->patient.getNationality()) << "\n";
+        out << "Address: " << QString::fromStdString(this->parent->patient.getAddress()) << "\n";
+
+        std::string sex = this->parent->patient.getSex() == Male ? "Male" : "Female";
+        out << "Sex: " << QString::fromStdString(sex) << "\n";
+
+        float height = this->parent->patient.getHeight();
+        out << "Height: " << QString::fromStdString(std::to_string(height)) << " cm\n";
+
+        float weight = this->parent->patient.getWeight();
+        out << "Weight: " << QString::fromStdString(std::to_string(weight)) << " kg\n";
+
+        file.close();
+
+        return true;
+    }
+    else return false;
 }
